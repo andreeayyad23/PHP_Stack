9f3a29c62778357368453e370b93f6dbce956df4
Andreh,ayyad <andreh,ayad@gmail.com>
2024-10-24 20:55:54 +0300

    Variabels

---
+++ Fundamentals/variabels.php
@@ -0,0 +1,25 @@
+<?php
+$name = "Andree";
+echo $name;
+
+$lastName = "Ayyad";
+echo $lastName;
+
+$string = "Majd";
+echo $string;
+
+$int = 24;
+echo $int;
+
+$float = 1.54;
+echo $float;
+
+$bool = TRUE;
+echo $bool;
+
+$array = [];
+echo empty($array);
+
+$object = NULL;
+echo $object;
+?>
